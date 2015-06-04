21551e96755b90b7ea84a304fbf6b024c00b3bcd
krujos <jkruck@pivotal.io>
2015-06-03 22:09:56 -0400

    Basic web functionality

--- app.py
+++ app.py
@@ -5,12 +5,19 @@ import json
 import requests 
 import sys
 import os
+from flask import Flask
+
+Flask.get = lambda self, path: self.route(path, methods=['get'])
 
 token = os.environ['CF_ACCESS_TOKEN']
 headers = {'Authorization': token}
 api = "https://api.10.244.0.34.xip.io"
 cache = dict() 
 
+port = 8003
+if 'PORT' in os.environ:
+    port = int(os.getenv("PORT"))
+    
 def cf(path):
     r = requests.get(api + path, headers=headers, verify=False)
     if r.status_code != 200: 
@@ -23,7 +30,12 @@ def api_cache(url):
         cache[url] = cf(url)
     return cache[url]
 
-def do_it():
+
+app = Flask(__name__)
+
+@app.get('/')
+def get_root():
+    response = "<html><body><pre>"
     apps = cf('/v2/apps')
 
     for app in apps['resources']:
@@ -40,12 +52,15 @@ def do_it():
             org = api_cache(space['entity']['organization_url'])
             routes = cf(app['entity']['routes_url'])
             
-            print("App: " + name + " Buildpack: " +  buildpack + " created at " + created_at + " updated at " + updated_at)
-            print("\tOrg: " + org['entity']['name'] + " Space: " + org['entity']['name'])
-            print ("Routes:")
+            response += str("App: " + name + " Buildpack: " +  buildpack + " created at " + created_at + " updated at " + updated_at)
+            response += str("\tOrg: " + org['entity']['name'] + " Space: " + org['entity']['name'])
+            response += ("Routes:")
             for route in routes['resources']:
                 host = route['entity']['host']
                 domain = api_cache(route['entity']['domain_url'])
-                print("\t" + host + "." + domain['entity']['name'])
-
-do_it()
+                response += ("\t" + host + "." + domain['entity']['name'])
+    response += "</pre></body></html>"
+    return response
+                
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port)
